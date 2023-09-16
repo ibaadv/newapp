7867e8a73df59aac71371081c90775d8c77d7116
Ibaad Rahman <ibaadrahmanv@gmail.com>
2023-09-16 11:24:37 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  function getData(val){
+    console.warn(val.target.value); 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get Input Box value !</h1>
+      <p>{data}</p>
+      <input type='text' onChange={getData}/>
     </div>
   );
 }
